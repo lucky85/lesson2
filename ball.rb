f555a1b200607a82f4cd2f790d9a261d7bc32759
lucky85 <lucky85@ukr.net>
2017-10-20 07:55:59 +0300

    ball done

---
+++ ball.rb
@@ -0,0 +1,14 @@
+require 'yaml'
+
+class Ball
+  ANSWERS = YAML.load_file('answers.yml')
+  def shake
+    answer = ANSWERS[rand(20)]
+    colors = [34, 32, 33, 31]
+    puts "\e[#{colors[ANSWERS.index(answer) / 5]}m#{answer}\e[0m"
+    answer
+  end
+end
+
+bal = Ball.new
+bal.shake
