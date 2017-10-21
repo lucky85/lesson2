7c9b8f04145b4bc2099089d3e4f4456e31f63901
lucky85 <lucky85@ukr.net>
2017-10-21 08:25:44 +0300

    done!

--- ball.rb
+++ ball.rb
@@ -2,11 +2,11 @@ require 'yaml'
 
 class Ball
   ANSWERS = YAML.load_file('answers.yml')
-
+  
   def shake
-    index = rand(20)
+    index = rand(ANSWERS.size - 1)
     answer = ANSWERS[index]
-    colors = [34, 32, 33, 31]
+    colors = [31, 32, 33, 34]
     puts "\e[#{colors[index / 5]}m#{answer}\e[0m"
     answer
   end
