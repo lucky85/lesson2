7248fc3552f008e2c3f387b597a365b49c1e07b4
Viacheslav <lucky85@ukr.net>
2017-10-21 08:22:06 +0300

    fixed
    fixed

--- ball.rb
+++ ball.rb
@@ -6,7 +6,7 @@ class Ball
   def shake
     index = rand(20)
     answer = ANSWERS[index]
-    colors = [34, 32, 33, 31]
+    colors = [31, 32, 33, 34]
     puts "\e[#{colors[index / 5]}m#{answer}\e[0m"
     answer
   end
